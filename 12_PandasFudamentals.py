f92be8b1a0bf4270070b3b909f2805c69c14eaa0
Tanishk <tanishkraj70@gmail.com>
2024-02-19 11:40:12 +0530

    commit

--- 12_PandasFudamentals.py
+++ 12_PandasFudamentals.py
@@ -11,4 +11,29 @@ print(d.shape)
 # Data Analytics Process
 '''
 1. Bussiness Problem: Process of Analytics begins with questions or business problems for stakeholders.
+2. Data Acquisition: Collect data from various sources for analysis to answer questions raised in step 1.
+3. Data Wrangling and Exploration: Data Wrangling includes data cleaning and data manipulation whereas data exploration includes data discovery and data pattern.
+   Data Wrangling Challenges:
+   -> Unexpected Data Format
+   -> Errorneous Data
+   -> Volumous Data to be manipulated
+   -> Classifying Data into linear or clusters
+   
+   Data Exploration:Model Selection
+   -> Based on overall data analysis process
+   -> Should be accurate to avoid iterations
+   -> Depends on pattern identification and algorithm
+   
+5. EDA(Exploratory Data Analysis):
+   -> Studies the data to recommend suitable model that best fits the data
+   EDA: Quatitative Technique
+   Measurement of Central Tendency
+   -> Mean: Suitable for symmetric distributions
+   -> Median: Suitable for skewed distributions and for catching outliers in the dataset.
+   -> Mode: Mode common value in the data
+   
+   EDA: Graphical technique-> Histograms and Scatter Plotts are most used techniques
+   
+6. Conclusions and Predictions: Reaching a conclusion and making predictions based on data Analysis.
+
 '''
